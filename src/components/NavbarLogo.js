1749abfc69e5b3498de5fed3604bb6249c690ac0
Leonardo Tricotti <tricottileo@gmail.com>
2023-03-14 16:40:40 -0300

    commit

--- src/components/NavbarLogo.js
+++ src/components/NavbarLogo.js
@@ -1,1 +1,11 @@
 import styles from "../css/navbarLogo.module.css";
+
+function NavbarLogo({ logo }) {
+  return (
+    <div className={styles.navbarLogo}>
+      <img src={logo} alt="Company Logo" />
+    </div>
+  );
+}
+
+export default NavbarLogo;
