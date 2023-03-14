11ed792d8bf64d5c47f38062040e289f1d6ea233
Leonardo Tricotti <tricottileo@gmail.com>
2023-03-14 17:15:09 -0300

    commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,57 @@
+import { NavLink } from "react-router-dom";
+import logo from "../assets/images/logo/navbar-logo.png";
+import styles from "../css/navbar.module.css";
+
+function NavbarLogo({ logo }) {
+  return (
+    <div className={styles.navbarLogo}>
+      <img src={logo} alt="Company Logo" />
+    </div>
+  );
+}
+
+function NavToggle() {
+  return (
+    <div className={styles.toggleContainer}>
+      <div className={styles.toggleLine} />
+      <div className={styles.toggleLine} />
+      <div className={styles.toggleLine} />
+    </div>
+  );
+}
+
+function NavItem({ url, text }) {
+  return (
+    <li className={styles.navItem}>
+      <NavLink className={styles.navLink} to={url}>
+        {text}
+      </NavLink>
+    </li>
+  );
+}
+
+function NavMenu() {
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles.navList}>
+        <NavItem url="/" text="Overview" />
+        <NavItem url="/payers" text="Employers & Payers" />
+        <NavItem url="/providers" text="Providers" />
+        <NavItem url="/press" text="Press" />
+        <NavItem url="/inquire" text="Request Information" />
+      </ul>
+    </nav>
+  );
+}
+
+function Navbar() {
+  return (
+    <header className={styles.navbar}>
+      <NavbarLogo logo={logo} />
+      <NavToggle />
+      <NavMenu />
+    </header>
+  );
+}
+
+export default Navbar;
