5095cdb5dfe5a228ca35aa456954656ff78ed055
Leonardo Tricotti <tricottileo@gmail.com>
2023-03-20 16:22:50 -0300

    commit

--- src/assets/data/feactureSectionData.js
+++ src/assets/data/feactureSectionData.js
@@ -6,6 +6,36 @@ const featureSectionData = [
     description:
       "In a historic move validating the nation's mental health crisis, the U.S. Preventive Services Task Force issued draft recommendations that adults under 65 get screened for anxiety.",
   },
+  {
+    id: 2,
+    image: URL("../images/feature-section/feature-two.png"),
+    title:
+      "Improve mental and physical health outcomes by providing access to enjoyable, evidence-based mental health content",
+    description:
+      "Our mental health programs are written by licensed clinicians and narrated by the voices of Calm. The programs adhere to the principles of Cognitive Behavioral Therapy, Acceptance & Commitment Therapy, and Dialectical Behavioral Therapy. ",
+  },
+  {
+    id: 3,
+    image: URL("../images/feature-section/feature-three.png"),
+    title: "Navigate members to the right level of care",
+    description:
+      "Our assessments may recommend Calm Health programs and/or direct your members to your provider network, specialty care, or other solutions based on acuity levels and pre-existing conditions.",
+  },
+  {
+    id: 4,
+    image: URL("../images/feature-section/feature-four.png"),
+    title: "Enhance care plan adherence",
+    description:
+      "Target concerns and encourage members to set goals, track medications and side effects, and easily share information with providers. Make it easier for members to stay on track.Target concerns and encourage members to set goals, track medications and side effects, and easily share information with providers. Make it easier for members to stay on track.",
+  },
+  {
+    id: 5,
+    image: URL("../images/feature-section/feature-five.png"),
+    title:
+      "Leverage active and passive monitoring for proactive outreach and engagement",
+    description:
+      "Our ability to capture user data from active sources like user check-ins to passive data capture from device integration allows us to proactively identify any mental health concerns and recommend the right care.",
+  },
 ];
 
 export default featureSectionData;
