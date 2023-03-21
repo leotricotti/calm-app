0f2d92734629dfaf55481ad506dda80c845fe6d8
Leonardo Tricotti <tricottileo@gmail.com>
2023-03-21 09:14:17 -0300

    commit

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,3 +1,48 @@
 import styles from "./additionalFeature.module.css";
 
-function additionalFeature() {}
+function MainTitle() {
+  return (
+    <div className={styles.mainTitleContainer}>
+      <h3 className={styles.mainTitle}>
+        Drive engagement with our easy-to-use platform
+      </h3>
+    </div>
+  );
+}
+
+function Icon({ icon }) {
+  return (
+    <div className={styles.iconContainer}>
+      <img src={icon} alt={icon} />
+    </div>
+  );
+}
+
+function Title({ title }) {
+  return (
+    <div className={styles.titleContainer}>
+      <h2 className={styles.title}>{title}</h2>
+    </div>
+  );
+}
+
+function Description({ description }) {
+  return (
+    <div className={styles.descriptionContainer}>
+      <p className={styles.description}>{description}</p>
+    </div>
+  );
+}
+
+function AdditionalFeature({ data }) {
+  return data.map((d) => {
+    return (
+      <div className={styles.additionalFeatureContainer}>
+        <MainTitle />
+        <Icon icon={d.icon} />
+        <Title title={d.title} />
+        <Description description={d.description} />
+      </div>
+    );
+  });
+}
