cc7379053946897300a649248edaffe78342e8f9
Leonardo Tricotti <tricottileo@gmail.com>
2023-03-21 20:16:36 -0300

    commit

--- src/components/FeaturesList.js
+++ src/components/FeaturesList.js
@@ -2,14 +2,6 @@ import { Fragment } from "react";
 import logo from "../assets/images/features-list/logo.png";
 import styles from "./featuresList.module.css";
 
-function FeatureListIcons({ icon, alt }) {
-  return (
-    <div className={styles.featuresListIconContainer}>
-      <img className={styles.featuresListIcon} src={icon} alt={alt} />
-    </div>
-  );
-}
-
 function MainTitle({ title }) {
   return (
     <div className={styles.mainTitleContainer}>
@@ -18,10 +10,18 @@ function MainTitle({ title }) {
   );
 }
 
-function FeatureListDescription({ description }) {
+function FeatureListDescription({ description, style }) {
   return (
-    <div className={styles.mainDescriptionContainer}>
-      <p className={styles.mainDescription}>{description}</p>
+    <div className={styles.descriptionContainer}>
+      <p className={style}>{description}</p>
+    </div>
+  );
+}
+
+function FeatureListIcons({ icon, alt }) {
+  return (
+    <div className={styles.featuresListIconContainer}>
+      <img className={styles.featuresListIcon} src={icon} alt={alt} />
     </div>
   );
 }
@@ -30,17 +30,20 @@ function FeaturesList({ data }) {
   return (
     <div className={styles.featuresListContainer}>
       <FeatureListIcons icon={logo} alt={"Company logotype"} />
-      <MainTitle mainTitle={"From the world's #1 brand for mental fitness"} />
+      <MainTitle title={"From the world's #1 brand for mental fitness"} />
       <FeatureListDescription
+        style={styles.firstChild}
         description={
           "Calm is the leading mental health brand with the #1 App for Sleep, Meditation and Relaxation™"
         }
       />
-
       {data.map((d) => (
         <Fragment key={d.id}>
           <FeatureListIcons icon={d.icon} alt={"Feature icon"} />
-          <FeatureListDescription description={d.description} />
+          <FeatureListDescription
+            description={d.description}
+            style={styles.restOfChilds}
+          />
         </Fragment>
       ))}
     </div>
