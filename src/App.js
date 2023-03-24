30458a5d999368fdbfc2edf10a922669fb9df02f
Leonardo Tricotti <tricottileo@gmail.com>
2023-03-24 13:42:05 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Payers from "./pages/Payers";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/payers" element={<Payers />} />
     </Routes>
   );
 }
---
+++ src/pages/Payers.jsx
@@ -0,0 +1,13 @@
+import NavHeader from "../components/NavHeader";
+import Footer from "../components/Footer";
+
+function Payers() {
+  return (
+    <div>
+      <NavHeader />
+      <Footer />
+    </div>
+  );
+}
+
+export default Payers;
