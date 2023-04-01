4ad2ff7d6e0d998340a3ff0243671ca659de5efa
Leonardo Tricotti <tricottileo@gmail.com>
2023-04-01 12:17:59 -0300

    commit

--- src/pages/Providers.jsx
+++ src/pages/Providers.jsx
@@ -1,4 +1,5 @@
 import NavHeader from "../components/NavHeader";
+import HeroSection from "../components/HeroSection";
 import BannerPurple from "../components/BannerPurple";
 import Footer from "../components/Footer";
 
@@ -6,6 +7,10 @@ function Providers() {
   return (
     <div>
       <NavHeader />
+      <HeroSection
+        title="Evidence-based solution that helps providers spend time with those who need it most"
+        subtitle="Calm Health offers evidence-based, condition-focused mental health programs that are aligned with providers advice and workflows."
+      />
       <BannerPurple
         title={"Want to learn more?"}
         text={"Request Information"}
