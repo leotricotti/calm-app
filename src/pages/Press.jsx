d93ce10d8f58f1618f26edc0129cf968b3c89c0d
Leonardo Tricotti <tricottileo@gmail.com>
2023-04-29 12:23:04 -0300

    commit

--- src/pages/Press.jsx
+++ src/pages/Press.jsx
@@ -1,10 +1,12 @@
 import NavHeader from "../components/NavHeader";
+import HeroSection from "../components/HeroSection";
 import Footer from "../components/Footer";
 
 function Press() {
   return (
     <div>
       <NavHeader />
+      <HeroSection />
       <Footer />
     </div>
   );
