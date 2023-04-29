75b6746b6805c190efd2245430afd98e00d22515
Leonardo Tricotti <tricottileo@gmail.com>
2023-04-29 10:49:28 -0300

    commit

--- src/assets/data/providersFeatureListData.js
+++ src/assets/data/providersFeatureListData.js
@@ -1,9 +1,28 @@
 const providersFeatureListData = [
   {
     id: 1,
-    title: "Complement your existing mental health programs and services",
+    title:
+      "A simple solution you can use to help patients with anxiety and depression",
     description:
-      "Our solutions offer simple and seamless integration into your current programs and standards of care. ",
+      "Our solutions seamlessly integrate into your current programs and health technology, meeting standards of care to drive better patient health outcomes.",
+  },
+  {
+    id: 2,
+    title: "Reduce administrative burden",
+    description:
+      "Reduce escalations around mental health by enabling access to self-directed content and tools.",
+  },
+  {
+    id: 3,
+    title: "Increase clinical team efficiency and member satisfaction",
+    description:
+      "Offer periodic PHQ-9 and GAD-7 surveys to track the patient’s progress.",
+  },
+  {
+    id: 4,
+    title: "Support collaborative care",
+    description:
+      "Connect professional care teams with members and families to reduce the communications gap.",
   },
 ];
 
