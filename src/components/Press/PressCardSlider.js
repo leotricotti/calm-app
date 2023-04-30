7fb5ae38bbf37151464806772b6c951a4fb01342
leotricotti <tricottileo@gmail.com>
2023-04-30 17:34:46 -0300

    commit

--- src/components/Press/PressCardSlider.js
+++ src/components/Press/PressCardSlider.js
@@ -3,12 +3,30 @@ import { NavLink } from "react-router-dom";
 import styles from "./pressCardSlider.module.css";
 import { useState } from "react";
 
-function Slide({ data }) {
+function Slide({ index, data }) {
   return (
     <div className={styles.slideContainer}>
       {data.map((data) => {
         return (
-          <div className={styles.slide} key={data.id}>
+          <div
+            className={styles.slide}
+            key={data.id}
+            style={{
+              transform: `translateX(${
+                index === 0
+                  ? "0"
+                  : index === 1
+                  ? "-100%"
+                  : index === 2
+                  ? "-200%"
+                  : index === 3
+                  ? "-300%"
+                  : index === 4
+                  ? "-400%"
+                  : "0"
+              })`,
+            }}
+          >
             <img src={data.image} alt="Slide item" className={styles.image} />
             <h2 className={styles.title}>{data.title}</h2>
             <p className={styles.text}>{data.text}</p>
@@ -22,35 +40,37 @@ function Slide({ data }) {
   );
 }
 
-function Dots({ handleClick }) {
+function Dots({ handleClick, index }) {
+  console.log(index);
+
   return (
     <div className={styles.dotsContainer}>
       <button
+        onClick={() => handleClick(0)}
+        className={`${styles.dots} ${index === 0 ? styles.active : ""}`}
+      ></button>
+      <button
         onClick={() => handleClick(1)}
-        className={`${styles.dots} ${styles.dotOne} ${styles.active}`}
+        className={`${styles.dots} ${index === 1 ? styles.active : ""}`}
       ></button>
       <button
         onClick={() => handleClick(2)}
-        className={`${styles.dots} ${styles.dotTwo}`}
+        className={`${styles.dots} ${index === 2 ? styles.active : ""}`}
       ></button>
       <button
         onClick={() => handleClick(3)}
-        className={`${styles.dots} ${styles.dotThree}`}
+        className={`${styles.dots} ${index === 3 ? styles.active : ""}`}
       ></button>
       <button
         onClick={() => handleClick(4)}
-        className={`${styles.dots} ${styles.dotFour}`}
-      ></button>
-      <button
-        onClick={() => handleClick(5)}
-        className={`${styles.dots} ${styles.dotFive}`}
+        className={`${styles.dots} ${index === 4 ? styles.active : ""}`}
       ></button>
     </div>
   );
 }
 
 function PressCardSlider() {
-  const [index, setIndex] = useState(1);
+  const [index, setIndex] = useState(0);
 
   const handleClick = (index) => {
     setIndex(index);
@@ -59,7 +79,7 @@ function PressCardSlider() {
   return (
     <>
       <Slide data={sliderData} index={index} />
-      <Dots handleClick={handleClick} />
+      <Dots handleClick={handleClick} index={index} />
     </>
   );
 }
