785d9f5734a4f12463b5922097be31a69e4aeeb9
Leonardo Tricotti <tricottileo@gmail.com>
2023-05-01 15:58:06 -0300

    commit

--- src/components/Press/PressCardSlider.js
+++ src/components/Press/PressCardSlider.js
@@ -3,39 +3,25 @@ import sliderData from "../../assets/data/sliderData";
 import { NavLink } from "react-router-dom";
 import styles from "./pressCardSlider.module.css";
 
-function Slide({ index, data }) {
+function Slide({ data, index }) {
+  let transformValue = `translateX(${-index * 100}%)`;
+
   return (
     <div className={styles.slideContainer}>
-      {data.map((data) => {
-        return (
-          <div
-            className={styles.slide}
-            key={data.id}
-            style={{
-              transform: `translateX(${
-                index === 0
-                  ? "0"
-                  : index === 1
-                  ? "-100%"
-                  : index === 2
-                  ? "-200%"
-                  : index === 3
-                  ? "-300%"
-                  : index === 4
-                  ? "-400%"
-                  : "0"
-              })`,
-            }}
-          >
-            <img src={data.image} alt="Slide item" className={styles.image} />
-            <h2 className={styles.title}>{data.title}</h2>
-            <p className={styles.text}>{data.text}</p>
-            <NavLink to={data.link} className={styles.linkContainer}>
-              <span className={styles.navLink}>{data.linkText}</span>
-            </NavLink>
-          </div>
-        );
-      })}
+      {data.map((item, i) => (
+        <div
+          className={styles.slide}
+          key={item.id}
+          style={{ transform: transformValue }}
+        >
+          <img src={item.image} alt="Slide item" className={styles.image} />
+          <h2 className={styles.title}>{item.title}</h2>
+          <p className={styles.text}>{item.text}</p>
+          <NavLink to={item.link} className={styles.linkContainer}>
+            <span className={styles.navLink}>{item.linkText}</span>
+          </NavLink>
+        </div>
+      ))}
     </div>
   );
 }
@@ -43,32 +29,13 @@ function Slide({ index, data }) {
 function Dots({ handleClick, index }) {
   return (
     <div className={styles.dotsContainer}>
-      <button
-        onClick={() => handleClick(0)}
-        className={`${styles.dots} ${index === 0 ? styles.active : ""}`}
-      ></button>
-      <button
-        onClick={() => handleClick(1)}
-        className={`${styles.dots} ${index === 1 ? styles.active : ""}`}
-      ></button>
-      <button
-        onClick={() => handleClick(2)}
-        className={`${styles.dots} ${styles.dotThree} ${
-          index === 2 ? styles.active : ""
-        }`}
-      ></button>
-      <button
-        onClick={() => handleClick(3)}
-        className={`${styles.dots} ${styles.dotFour} ${
-          index === 3 ? styles.active : ""
-        }`}
-      ></button>
-      <button
-        onClick={() => handleClick(4)}
-        className={`${styles.dots} ${styles.dotFive} ${
-          index === 4 ? styles.active : ""
-        }`}
-      ></button>
+      {[...Array(sliderData.length)].map((_, i) => (
+        <button
+          key={i}
+          onClick={() => handleClick(i)}
+          className={`${styles.dots} ${index === i ? styles.active : ""}`}
+        ></button>
+      ))}
     </div>
   );
 }
@@ -82,7 +49,9 @@ function PressCardSlider() {
 
   return (
     <>
-      <Slide data={sliderData} index={index} />
+      <div className={styles.pressCardSliderContainer}>
+        <Slide data={sliderData} index={index} />
+      </div>
       <Dots handleClick={handleClick} index={index} />
     </>
   );
